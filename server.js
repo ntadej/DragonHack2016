e329c8c52a661c7f25bb07ce907ea571b9e2d236
Tadej Novak <tadej@tano.si>
2016-05-14 22:16:43 +0200

    Server fixes

--- server.js
+++ server.js
@@ -4,65 +4,84 @@ var http = require('http').Server(app);
 var io = require('socket.io').listen(http);
 var fs = require('fs');
 
+// Add headers
+app.use(function(req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+
 var dirs = ["l", "r", "f", "b", "s"];
 var listOfBroadcasts = {};
-var cars = 
-    {
-        "cars":
-            [
-                {
-                    "id": 1,
-                    "name": "Abraham"
-                },
-                {
-                    "id": 2,
-                    "name": "Oliver"
-                }
-            ]
+var cars = {
+        "cars": [{
+            "id": 1,
+            "name": "Abraham"
+        }, {
+            "id": 2,
+            "name": "Oliver"
+        }]
     }
-/**
- * Serves index.html on route '/'
- */
-app.get('/', function(req, res){
+    /**
+     * Serves index.html on route '/'
+     */
+app.get('/', function(req, res) {
     res.sendFile(__dirname + '/public/index.html');
 });
 
-app.get('/api/cars', function(req, res){
+app.get('/api/cars', function(req, res) {
     res.json(cars);
 });
 
-app.get('/api/cars/1', function(req, res){
-    res.json(cars.cars[0]);
+app.get('/api/cars/1', function(req, res) {
+    res.json({
+        "car": cars.cars[0]
+    });
 });
 
-app.get('/api/cars/2', function(req, res){
-    res.json(cars.cars[1]);
+app.get('/api/cars/2', function(req, res) {
+    res.json({
+        "car": cars.cars[1]
+    });
 });
 
 app.use(express.static('public'));
 
-app.use(function(req, res){
-   res.redirect('/');
+app.use(function(req, res) {
+    res.redirect('/');
 });
 
 /**
  * Listens to user connect event
  */
-io.on('connection', function(socket){
+io.on('connection', function(socket) {
     console.log('a user connected');
 
     /**-
-     * Listens to user disconnect 
+     * Listens to user disconnect
      * event (default event)
      */
-    socket.on('disconnect', function(){
+    socket.on('disconnect', function() {
         console.log('user dcd');
     });
 
     /**
      * Listens to event called 'test'
      */
-    socket.on('test', function(msg){
+    socket.on('test', function(msg) {
         console.log(msg);
         io.sockets.emit('action', msg);
     });
@@ -73,15 +92,15 @@ io.on('connection', function(socket){
      *
      * l = left; r = right; f = forward; b = back; s = stop
      */
-    setInterval(function(){
-        var dir = dirs[Math.floor(Math.random()*dirs.length)];
+    setInterval(function() {
+        var dir = dirs[Math.floor(Math.random() * dirs.length)];
 
         io.sockets.emit('action', dir);
     }, 7000);
 
 });
 
-http.listen(3000, function(){
+http.listen(3000, function() {
     console.log('*3000');
 });
 
