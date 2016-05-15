954ed79138721c74b465575a7881984523b8d61f
Tadej Novak <tadej@tano.si>
2016-05-15 11:07:43 +0200

    Add accelerometer detection

--- client/app/routes/driver/car.js
+++ client/app/routes/driver/car.js
@@ -13,6 +13,57 @@ export default Ember.Route.extend({
 
       container.append('<div class="driver-wrapper"><video class="driver left" src="' + video.attr('src') + '" autoplay></video></div>');
       container.append('<div class="driver-wrapper"><video class="driver right" src="' + video.attr('src') + '" autoplay></video></div>');
+
+      if (!window.DeviceMotionEvent) {
+        console.log('Device does not support motion');
+      } else {
+        var degreesToChange = 25; // How much does the phone have to turn, to change the direction [°]
+        var firstPositionFlag = true; // Flag to know, if we need to set first position
+        var firstPosition; // First position. Direction change depends on this
+
+        function getDiff(pos1, pos2) {
+          return (pos2 - pos1 + 540) % 360 - 180;
+        }
+
+        window.ondeviceorientation = function(event) {
+          var alpha = Math.round(event.alpha);
+          var beta = Math.round(event.beta);
+          var gamma = Math.round(event.gamma);
+
+          var alphaDiff, betaDiff, gammaDiff;
+
+          if (firstPositionFlag) {
+            firstPositionFlag = false;
+            firstPosition = {
+              "alpha": alpha,
+              "beta": beta,
+              "gamma": gamma
+            };
+          } else {
+            if (gamma >= 0) {
+              alphaDiff = getDiff(firstPosition.alpha, alpha + 180);
+            } else {
+              alphaDiff = getDiff(firstPosition.alpha, alpha);
+            }
+            betaDiff = getDiff(firstPosition.beta, beta);
+            gammaDiff = getDiff(firstPosition.gamma, gamma);
+
+            if (gamma > 0 && gamma < 50) {
+              socket.emit('directionChange', 's');
+            } else if (gamma < 0 && gamma > -50) {
+              socket.emit('directionChange', 'b');
+            } else {
+              if (alphaDiff > degreesToChange) {
+                socket.emit('directionChange', 'l');
+              } else if (alphaDiff < -degreesToChange) {
+                socket.emit('directionChange', 'r');
+              } else {
+                socket.emit('directionChange', 'f');
+              }
+            }
+          }
+        }
+      }
     });
     controller.set('connection', connection);
   },
