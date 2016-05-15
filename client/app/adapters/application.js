ed8975c1b83bae91a06938151181e41888567622
Tadej Novak <tadej@tano.si>
2016-05-15 08:36:58 +0200

    HTTPS try 2

--- client/app/adapters/application.js
+++ client/app/adapters/application.js
@@ -2,5 +2,5 @@ import RESTAdapter from 'ember-data/adapters/rest';
 
 export default RESTAdapter.extend({
   namespace: 'api',
-  host: 'http://dragon.pyphy.com'
+  host: 'https://dragon.pyphy.com'
 });
