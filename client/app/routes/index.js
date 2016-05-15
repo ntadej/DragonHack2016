6fea1aae8351c35dfc46127e78ba3d9865679b6c
Tadej Novak <tadej@tano.si>
2016-05-15 10:06:45 +0200

    Style - part 1

--- client/app/routes/index.js
+++ client/app/routes/index.js
@@ -1,3 +1,11 @@
 import Ember from 'ember';
 
-export default Ember.Route.extend();
+export default Ember.Route.extend({
+  onActivate: function() {
+    Ember.$('body').addClass('index');
+  }.on('activate'),
+
+  onDeactivate: function() {
+    Ember.$('body').removeClass('index');
+  }.on('deactivate')
+});
